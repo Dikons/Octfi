3f04fb8a8e395683cebac814f6306e39cb81ca94
Student <Student@MB310-87>
2016-09-27 10:18:43 +0300

    Add project files.

---
+++ src/AndreiProj1/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿//Text with JS
+//var myElement = document.getElementById("thought");
+//myElement.innerHTML = "Done by js";
+
+//Text with jQuery
+var myElement = $("#thought");
+myElement.text("This is by jQuery");
+
+
+//Events with JS
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style = "background: #888";
+}
+
+main.onmouseleave = function () {
+    main.style = "";
+}
+
+//Events with jQuery:
+var main = $("#main");
+main.on("mouseenter", function () {
+    $("#main").css("background-color", "yellow")
+});
+main.on("mouseleave", function () {
+    $("#main").css("background-color", "")
+});
